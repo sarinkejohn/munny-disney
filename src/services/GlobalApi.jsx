9aee431855d6841b1bb4fb4e289193591c4a8292
sarinkejohn <sarinkejohn@gmail.com>
2023-10-17 13:31:18 +0300

    Second commit

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -3,9 +3,6 @@ import axios from "axios";
 const movieBaseUrl = import.meta.env.VITE_GLOBAL_URL
 const api_key = import.meta.env.VITE_TMDB_API_ACCESS
 
-// const api_key = "2ec0d66f5bdf1dd12eefa0723f1479cf";
-// const movieBaseUrl = "https://api.themoviedb.org/3";
-
 const movieByGenreBaseURL = import.meta.env.VITE_GENRE_URL
 
 const getTrendingVideos = axios.get(
